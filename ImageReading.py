c189117e42f3e8ea4cb3738c442f60672c5ca49d
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-07 08:22:00 -0500

    Update ImageReading.py

--- ImageReading.py
+++ ImageReading.py
@@ -1,1 +1,27 @@
+#Imports necessary packages
+import cv2 as cv
 
+#Imports function for reading images
+import ImageProcessing
+import ImageDisplaying
+
+def readImage(image):
+    #Variable needed for displaying the video
+    #https://geeksforgeeks.org/python-play-a-video-using-opencv/ lines 15 - 20
+    videoIsPlaying = True
+
+    #Starts the video capture
+    video = cv.VideoCapture(image)
+
+    #Prevents program from crashing error
+    try:
+        #While the video is playing, read the frame, process it & display it
+        while videoIsPlaying == True:
+            videoIsPlaying, frame = video.read()
+            ImageProcessing.processImage(frame)
+            ImageDisplaying.displayImage(frame)
+        cv.destroyAllWindows()
+
+    #Prints message when video is done
+    except: print("The video is done.")
+    finally: exit()

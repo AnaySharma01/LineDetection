7f579596f478f6309889f712787019160d74056a
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-07 08:13:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Imports necessary packages
+import cv2 as cv
+from tkinter import *
+from PIL import Image, ImageTk
+import numpy as np
+
+#Image reading function
+
+#Image processing function
+
+#Image displaying function

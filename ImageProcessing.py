d07a3f2b58999c9a6446385ab9aa9035c1bdc877
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-08 08:31:05 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,15 +3,29 @@ import cv2 as cv
 import numpy as np
 
 def processImage(image):
-    # Applies gaussian and median blur
+    #Creates mask
+    #https://pyimagesearch.com/2021/01/19/image-masking-with-opencv/ Lines 20-26
+    roi = np.zeros(image.shape[:2], dtype="uint8")
+    cv.rectangle(roi, (50, 90), (300, 600), 255, -1)
+    mask = cv.bitwise_and(image, image, mask=roi)
+
+    #Applies gaussian and median blur
     #https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Lines 35-38
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
     gray_scale = cv.GaussianBlur(gray, (15, 15), 0)
     median_blur = cv.medianBlur(gray_scale, 5)
-    canny_image = cv.Canny(median_blur, 130, 220)
+    canny_image = cv.Canny(median_blur, 120, 120)
 
-    # Creates hough lines around image
+    #Creates hough lines around image
     #https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 42
-    lines = cv.HoughLinesP(canny_image, 1, np.pi / 180, threshold=10, minLineLength=15, maxLineGap=2)
+    lines = cv.HoughLinesP(canny_image, 25, np.pi / 180, threshold=120, minLineLength = 0, maxLineGap=0)
 
-
+    #Displays hough lines
+    #https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 14-19
+    lines_list = []
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv.line(image, (x1, y1), (x2, y2), (0, 255, 0), 10)
+            lines_list.append([(x1, y1), (x2, y2)])
+        return line

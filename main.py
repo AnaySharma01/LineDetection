c2cdb301b304f450ae24d961373ca2cab1211532
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-10 11:21:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-#Imports necessary functions
-import ImageReading
+# Imports necessary packages
+import cv2 as cv
 
-#Loads the image, processes it and displays it
-ImageReading.readImage()
+# Imports functions for processing and displaying the frames
+import ImageProcessing
+import ImageDisplaying
 
+
+def DisplayLines():
+    # https://geeksforgeeks.org/python-play-a-video-using-opencv/ lines 15 - 20
+    # Variable needed for displaying the video
+    videoIsPlaying = True
+
+    # Starts the video capture
+    video = cv.VideoCapture(0)
+
+    # While the video is playing, read the frame, process it & display it
+    while videoIsPlaying == True:
+        videoIsPlaying, frame = video.read()
+        ImageProcessing.processImage(frame)
+        ImageDisplaying.displayImage(frame)
+    #Destroys the program when exiting
+    cv.destroyAllWindows()
+  
+#Runs the program
+DisplayLines()
